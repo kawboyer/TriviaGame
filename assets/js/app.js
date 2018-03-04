4ac8e154872e006e2523b1575ba48d0675a0c2bb
kawboyer <kawboyer@gmail.com>
2018-03-04 18:01:33 -0500

    more content

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $(".start-playing-button").click(function() {
+       alert("You clicked the button!");
+       $(".initially-hidden-block").show();
+       $(".start-playing-button").hide();
+       
+
+    });
+
+    console.log("js is linked");
+});
